984e08ba3fc5bf6965e091ea93e2465c5c8df5fb
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-04 15:39:16 +0200

    Creato un cilco per stampare 100 numeri in console

---
+++ script.js
@@ -0,0 +1,12 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi "Fizz" al posto del numero,
+// per i multipli di 5, stampi "Buzz".
+//Per i multipli sia di 3 che di 5, stampa 'FizzBuzz'
+
+// creo un ciclo che mi stampi 100 numeri
+var i = 1;
+while (i <= 100) {
+
+  console.log(i);
+  i++;
+}
